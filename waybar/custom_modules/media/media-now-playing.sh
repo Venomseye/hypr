ddfe932f4fd617b40e01108ea3bec738ae5c8a6f
Venomseye <103646076+Venomseye@users.noreply.github.com>
2026-07-31 16:38:55 +0530

    Refactor media-now-playing.sh to remove wait command

--- waybar/custom_modules/media/media-now-playing.sh
+++ waybar/custom_modules/media/media-now-playing.sh
@@ -5,6 +5,4 @@ DIR="$(dirname "$0")"
 zscroll -l 20 \
     --delay 0.3 \
     --update-check true \
-    "$DIR/media-now-playing-info.sh" 2>/dev/null | "$DIR/media-now-playing-wrap.sh"
-
-wait
+    "$DIR/media-now-playing-info.sh" | "$DIR/media-now-playing-wrap.sh"
